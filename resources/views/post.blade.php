a09ed129ef0389e9d747e44e99c57a88df78baf2
Courtney <jcourtney@edsal.com>
2021-07-19 20:39:11 -0500

    Episode 8 - Store Blog Posts as HTML Files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem maxime laborum doloremque rerum! Consequatur similique, odio facere itaque sequi error facilis dolorem, quaerat labore voluptates tempore, sit unde dolorum ad.
-        </p>
+        <?= $post; ?>
     </article>
     <a href="/">Go Back</a>
 </body>
