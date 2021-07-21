ea9b5f0e90f3818ff10958e2d526f9f3c2d113be
Courtney <jcourtney@edsal.com>
2021-07-21 17:26:54 -0500

    Episode 20 - Make a Post Model and Migration

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
+        <article>
             <h1>
-                <a href="posts/{{ $post->slug }}">
+                <a href="posts/{{ $post->id }}">
                     {{ $post->title }}
                 </a>
             </h1>
