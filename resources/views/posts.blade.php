956fa55b856d418999142a0ef614a4ff808f5061
Courtney <jcourtney@edsal.com>
2021-07-21 20:50:43 -0500

    Episode 25 - Show All Posts Associated With a Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,9 +8,9 @@
             </h1>
 
             <p>
-                <a href="#">{{ $post->category->name }}</a>
+                <a href="categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
-            
+
             <div>
                 {{ $post->excerpt }}
             </div>
