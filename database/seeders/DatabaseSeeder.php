3364d279e7fcfca71b73523bd6f8ef77219d7539
Courtney <jcourtney@edsal.com>
2021-07-21 21:51:49 -0500

    Episode 28 - Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aut illo id incidunt consequuntur iste ducimus aspernatur esse facere nam fugiat accusamus, doloremque ex possimus, doloribus blanditiis voluptates architecto temporibus!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aut illo id incidunt consequuntur iste ducimus aspernatur esse facere nam fugiat accusamus, doloremque ex possimus, doloribus blanditiis voluptates architecto temporibus!</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
