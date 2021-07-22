ca6c9cfe11074728ab027cb6c8474b98aa82324d
Courtney <jcourtney@edsal.com>
2021-07-21 21:26:54 -0500

    Episode 27 - Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aut illo id incidunt consequuntur iste ducimus aspernatur esse facere nam fugiat accusamus, doloremque ex possimus, doloribus blanditiis voluptates architecto temporibus!</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aut illo id incidunt consequuntur iste ducimus aspernatur esse facere nam fugiat accusamus, doloremque ex possimus, doloribus blanditiis voluptates architecto temporibus!</p>'
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,11 +8,11 @@
             </h1>
 
             <p>
-                <a href="categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
 
             <div>
-                {{ $post->excerpt }}
+                {!! $post->excerpt !!}
             </div>
         </article>
     @endforeach
